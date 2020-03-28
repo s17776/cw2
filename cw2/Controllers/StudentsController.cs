56ce2eb78aef6747ed99e404a5c169fb3e82a573
Marek Palasek <s17776@pjwstk.edu.pl>
2020-03-28 10:53:36 +0100

    dodałem kod

---
+++ cw2/Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw2.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        /* cw 4
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            
+
+            return NotFound("Nie znaleziono studenta");
+        }
+    
+          cw 5   */
+
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+
+
+
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+
+    }
+}
